8856ac3375d656989e5bb5105c8abe5f55433c56
Vaishanavi Shelke <deshmukhrushikesh684@gmail.com>
2026-03-09 18:28:00 +0530

    db public url

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -2,7 +2,7 @@ import { Sequelize } from 'sequelize';
 import dotenv from 'dotenv';
 
 dotenv.config();
-const sequelize = new Sequelize(process.env.DATABASE_URL, {
+const sequelize = new Sequelize(process.env.DATABASE_PUBLIC_URL, {
   dialect: "postgres",
   logging: false,
   dialectOptions: {
